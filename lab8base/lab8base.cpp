9ee41d4db6edc480c27812b7f0a2693fa9df51fc
kote6725@gmail.com <kote6725@gmail.com>
2019-06-07 00:56:36 +0300

    Добавьте файлы проекта.

---
+++ lab8base/lab8base.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <list>
+
+int main()
+{
+	std::list<float> flist{ 1,2,3,-1,3,-4,4,-4,-2,5,5 };
+	std::list<float> reslist;
+	bool flag = false;
+	for (auto& iter : flist)
+	{
+		if (!flag)
+		{
+			reslist.push_back(iter);
+		}
+		flag = iter < 0;
+	}
+	for (auto& iter : reslist)
+	{
+		std::cout << iter << '\n';
+	}
+}
